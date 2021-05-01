d332c662c78cce81d7611ca0bce465b3a735ef66
EsraaKhalifa <EsraaKhalifa15576@azhar.edu.eg>
2021-05-01 21:56:35 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
-const customerRoutes = require('./CustomerRoutes')
+const customerRoutes = require('./routes/CustomerRoutes')
 
 //var db="mongodb+srv://noorulhoda:no3011997@cluster0.z7j3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 var db='mongodb+srv://khairokom:khairokom@cluster0.jo718.mongodb.net/khairokom'
