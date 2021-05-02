7538ea930bbfe37f8abfb49288eb6718498e74ac
Fatma Kamel <nkamel22@outlook.com>
2021-05-02 23:19:50 +0000

    Added CommentController.js to /controllers

---
+++ controllers/CommentController.js
@@ -0,0 +1,37 @@
+const Comment = require('./Comment.model');
+
+module.exports = {
+
+  all(req, res, next){
+    const limit = parseInt(req.query.limit) || ''
+    Comment.find({}).limit(limit)
+    .then(comments => res.status(200).send(comments))
+    .catch(next)
+  },
+
+  create(req, res, next){
+    const commentProps = req.body;
+    console.log(commentProps);
+    Comment.create(commentProps)
+      .then(comment =>
+        res.status(201).send(comment)) 
+      .catch(next) 
+
+  },
+
+ edit(req, res, next){
+    const CommentId = req.params.id;
+    const commentProps = req.body;
+    Comment.findByIdAndUpdate({_id: commentId}, commentProps)
+    .then(() => Comment.findById({_id: commentId}))
+    .then(comment => res.status(200).send(comment))
+    .catch(next);
+  },
+
+  delete(req, res, next){
+    const commentId = req.params.id;
+    Comment.findByIdAndRemove({_id: commentId})
+      .then(comment => res.status(204).send(comment))
+      .catch(next);
+  }
+};
