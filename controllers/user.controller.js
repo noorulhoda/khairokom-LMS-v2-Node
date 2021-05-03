63ea38ec2e34bb51fa54d9cb2a0a03e45b473b02
EsraaKhalifa <EsraaKhalifa15576@azhar.edu.eg>
2021-05-03 20:43:18 +0000

    Updated user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,6 +19,42 @@ module.exports = {
 
   },
 
+  /*register(req,res,next){
+    let users=[];
+    const limit = parseInt(req.query.limit) || ''
+    user.find({}).limit(limit)
+    .then(usrs => users=usrs)
+    .catch(next)
+    users.forEach(account => {
+        if (account.email == req.body.email) {
+            return
+        }
+    })
+    users.push(req.body);
+    console.log('registeredSuccessfully')
+  }
+,*/
+login(req,res,next){
+    console.log("logging");
+    let users= this.all;
+    user.find({} , (err, users) => {
+        if(err) console.log('erroooooor');
+        users.map(account => {
+            console.log(account.userName +"  "+ req.body.userName)
+            if (account.userName == req.body.userName && account.password == req.body.password) {
+                console.log("logined successfully");
+               
+            }
+            else{
+                console.log('you are not registered')
+            }
+            return
+        })
+    })
+}
+
+
+,
  edit(req, res, next){
     const userId = req.params.id;
     const userProps = req.body;
