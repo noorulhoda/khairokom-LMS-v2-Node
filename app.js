110a5e1a70c98ffc3f4f0ede1fb8e9576d856c1d
nawal zaki <nawalzaki@outlook.com>
2021-05-03 12:35:49 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
 const customerRoutes = require('./routes/CustomerRoutes')
 const userRoutes=require('./routes/user.route')
+const categoryRoutes=require('./routes/category.route')
+const classRoutes=require('./routes/class.route')
 
 var db='mongodb+srv://khairokom:khairokom@cluster0.jo718.mongodb.net/khairokom'
 mongoose.connect(db,{useNewUrlParser: true,  useUnifiedTopology: true});
@@ -17,6 +19,8 @@ app.listen(port,function(){
 app.use(bodyParser.json());
 customerRoutes(app)
 userRoutes(app)
+categoryRoutes(app)
+classRoutes(app)
 //middleWares
 app.use((err, req, res, next)=>{
   res.status(422).send({error: err.message})
