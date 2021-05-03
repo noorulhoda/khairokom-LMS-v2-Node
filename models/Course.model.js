bdff83e4d9aad9573b2b406a193b15b03261e528
Fatma Kamel <nkamel22@outlook.com>
2021-05-03 00:48:05 +0000

    Added Course.model.js to /models

---
+++ models/Course.model.js
@@ -0,0 +1,29 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var CourseSchema=new Schema({
+tittle:
+{
+    type: String,
+    required: true
+},
+description:
+{
+    type: String,
+    required: true
+},
+image:
+{
+    type: String,
+    required:true
+},
+categoryID:
+{
+    type:String,
+    required:true
+}
+
+})
+
+const Course = mongoose.model('Course', CourseSchema);
+module.exports = Course;
