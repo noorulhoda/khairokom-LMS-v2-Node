4843fb0cbe7737222ef7cef84ffc6a7ca88360a2
nawal zaki <nawalzaki@outlook.com>
2021-05-03 12:32:08 +0000

    Updated category.route.js

---
+++ routes/category.route.js
@@ -0,0 +1,15 @@
+const categoryController  = require('../controllers/category.controller');
+
+module.exports = (app) =>{
+  app.get('/api/category',
+    categoryController.all);
+
+  app.post('/api/category',
+    categoryController.create);
+
+  app.put('/api/category/:id',
+   categoryController.edit);
+
+  app.delete('/api/catgeory/:id',
+   categoryController.delete)
+}
