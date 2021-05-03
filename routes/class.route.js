cc6f7679e96cebc35506f41c73769012b746674f
nawal zaki <nawalzaki@outlook.com>
2021-05-03 12:33:57 +0000

    Updated class.route.js

---
+++ routes/class.route.js
@@ -0,0 +1,15 @@
+const classController  = require('../controllers/class.controller');
+
+module.exports = (app) =>{
+  app.get('/api/class',
+    classController.all);
+
+  app.post('/api/class',
+    classController.create);
+
+  app.put('/api/class/:id',
+   classController.edit);
+
+  app.delete('/api/class/:id',
+   classController.delete)
+}
