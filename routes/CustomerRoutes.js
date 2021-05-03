61ec052e90283b50da4cf62cb8c34fab8215aa1d
EsraaKhalifa <EsraaKhalifa15576@azhar.edu.eg>
2021-05-03 23:14:37 +0000

    Updated CustomerRoutes.js

--- routes/CustomerRoutes.js
+++ routes/CustomerRoutes.js
@@ -5,7 +5,7 @@ module.exports = (app) =>{
     CustomerController.all);
 
    app.get('/', function(req, res){
-      res.send('hellooooooooooo')
+      res.send('khairokomAPI')
   });
   app.post('/api/customer',
     CustomerController.create);
