617b5a96faa8416a19c488329a869cf6356c3929
noor ulhoda <noooralhoda2030@gmail.com>
2021-05-07 18:27:36 +0200

    cors problem solved :)

--- app.js
+++ app.js
@@ -21,22 +21,25 @@ app.listen(port,function(){
     console.log('listening.....'+port);
 })
 
-/*app.use(cors());
+app.use(function (req, res, next) {
 
-var allowedDomains = ['http://localhost:8080'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // bypass the requests with no origin (like curl requests, mobile apps, etc )
-    if (!origin) return callback(null, true);
- 
-    if (allowedDomains.indexOf(origin) === -1) {
-      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-*/
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://khairokom33.azurewebsites.net');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(bodyParser.json());
 app.use('', authRoute)
 customerRoutes(app)
