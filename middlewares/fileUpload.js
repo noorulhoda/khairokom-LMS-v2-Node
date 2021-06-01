020c555e7b5d4395b98248166ca45bbc20443072
Nawal Zaki <nawalzaki333@gmail.com>
2021-06-01 14:48:31 +0200

    update API

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -1,7 +1,7 @@
 const util = require("util");
 const multer = require("multer");
 
-const DIR = './uploads/';
+const DIR = './public/uploads/';
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
